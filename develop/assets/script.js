4d41d3201f7eaf656b7678a8f094cca413d3f058
Victoria <sweet.victoria1218@gmail.com>
2023-08-19 09:17:06 -0500

    cloned starter code into repo

---
+++ develop/assets/script.js
@@ -0,0 +1,26 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+function generatePassword() {
+  let password = "444";
+  console.log("generate password here")
+  return password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
