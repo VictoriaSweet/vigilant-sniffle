f30ade56a51b67fd307aa53403718e337e4b517b
VictoriaSweet <sweet.victoria1218@gmail.com>
2023-08-24 12:06:15 -0500

    added for loop and defined password

--- develop/assets/script.js
+++ develop/assets/script.js
@@ -19,31 +19,38 @@ function getRandomCharacterFromArray(array) {
 // WHEN I answer each prompt
 // THEN my input should be validated and at least one character type should be selected
 function generatePassword() {
-  let password = "";
-
-  const length = 8;
+  let password = [];
+  let letter = [];
   const lowercaseCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
   const uppercaseCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   const numericCharacters = ['1','2','3','4','5','6','7','8','9','0'];
   const specialCharacters = ['!','@','#','$','%','^','&','*','(',')','/','|','"','.',',','?','<','>'];
-  //let  CharacterCount = >8 , <128?
- // if (keyLength < 8 || keyLength > 128) {
-    //alert("Your password does not meet the critia");
-  //  var keyLength = prompt("Password must be between 8 and 128 characters in length.");
- // }
+ // let  CharacterCount = >8 , <128
+  var length = prompt("Password must be between 8 and 128 characters in length.");
+  if (length < 8 || length > 128) {
+  alert("Your password does not meet the critia");
+   return null;
+ }
 
   // make a prompt that asks if they want lowercase
   let hasLowercase = confirm("Do you want lowercase?");
   let hasUppercase= confirm("Do you want uppercase?");
   let hasNumeric = confirm("Do you want numbers?");
   let hasSpecialCharacters = confirm("Do you want special characters?");
-  let characterCount= prompt("please select what character count is required. Min 8  Max 128");
+  const atLeastOneCharacterTypeSelected =
+   hasLowercase || hasUppercase || hasNumeric || hasSpecialCharacters;
+  if (!atLeastOneCharacterTypeSelected) {
+    alert("You did not select at least one character type.");
+    return null;
+  }
+  
+  //let characterCount= prompt("please select what character count is required. Min 8  Max 128");
 
   if (hasLowercase) {
-    const letter = getRandomCharacterFromArray(lowercaseCharacters);
+    letter.push(getRandomCharacterFromArray (lowercaseCharacters));
     console.log(letter)
     console.log(  password.concat(letter))
-    password=password.concat(letter)
+    password=password.concat(lowercaseCharacters)
     console.log(password)
   
   }
@@ -71,24 +78,24 @@ function generatePassword() {
     password=password.concat(letter)
     console.log(password)
   }
+var final = []
+var options = {
+  length:length
+}
+return options
+//do a for loop on length, in the logic input the possibility of password
+for (let i = 0; i < options.length; i++) {
+  var possible = getRandomCharacterFromArray(password);
+  final.push(possible)
+}
 
-
-  const atLeastOneCharacterTypeSelected =
-    hasLowercase || hasUppercase || hasNumeric || hasSpecialCharacters;
-
-  if (!atLeastOneCharacterTypeSelected) {
-    alert("You did not select at least one character type.");
-    
-  }
-
-  return password;
 }
 
 
 // Write password to the #password input
 function writePassword() {
   // create an alert here that tells users to decide their prompts
-  alert("Please select character type requirements for password")
+ // alert("Please select character type requirements for password")
   const password = generatePassword();
   const passwordText = document.querySelector("#password");
 
