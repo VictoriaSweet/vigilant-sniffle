f6d0a0510a620b226d005100e0c806b51b988272
Victoria <sweet.victoria1218@gmail.com>
2023-08-28 16:10:05 -0500

    rephrased prompts, detailed prompt 1

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,7 @@ function generatePassword() {
   const uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   const numericCharacters = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
   const specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '/', '|', '"', '.', ',', '?', '<', '>'];
-  var length = prompt("Password must be between 8 and 128 characters in length.");
+  var length = prompt("Enter the amount of characters required between 8 and 128 characters in length. (Using only digits)  i,e; 1, 2, 3");
   if (length < 8 || length > 128) {
     alert("Your password does not meet the criteria");
     return password;
